877ae94791f2d42035231dee0cf81335bb5a597c
m-jurzyk <jurzyk.maciej@gmail.com>
2023-07-24 09:39:18 +0200

    Update: dodana kolejna część danych zrobiona nieco innym - szybszym sposobem

--- Evapotranspiration.R
+++ Evapotranspiration.R
@@ -9,21 +9,25 @@ library(tidyverse)
 tabela1
 
 
-# Lista upraw, które chcesz zagregować
+# lista upraw
+
 uprawy_do_agregacji <- c("rzepak ozimy", "pszenica ozima", "buraki cukrowe", "kukurydza", "laka")
 
-# Przekształcenie danych do postaci długiej (long format) i zagregowanie danych
+# Agregowanie danych
+
 pola2_long <- reshape2::melt(tabela1, id.vars = c("uprawa", "NR_POLA"), measure.vars = paste0("X", num_dni))
 pola2_agregowane <- subset(pola2_long, uprawa %in% uprawy_do_agregacji)
 pola2_agregowane <- aggregate(value ~ variable + uprawa, data = pola2_agregowane, mean) # Uśrednienie wartości
+
+pola2_agregowane
+
 
 uprawy <- ggplot(pola2_agregowane, aes(x = variable, y = value, color = uprawa, group = uprawa)) +
   geom_line() +
   labs(title = "Zmiany wysokości upraw w kolejnych dniach",
        x = "Numer dnia roku",
        y = "Średnia wysokość uprawy") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme_minimal()
 
 uprawy
 
@@ -32,11 +36,11 @@ mapowanie_dni <- c("15 czerwca", "18 czerwca", "19 czerwca", "20 czerwca", "24 c
 uprawy1 <- ggplot(pola2_agregowane, aes(x = variable, y = value, color = uprawa, group = uprawa)) +
   geom_line() +
   labs(title = "Zmiany w ewapotranspiracji upraw obserwowanych w dniach 15.06.2022 - 24.06.2022 w godzinach 6:30-10:00",
-       x = "Data",
-       y = "Ewapotranspiracja z powierzchni liści rośliny) +
+       x = "Czas pomiaru",
+       y = "Średnia ewapotranspiracja z powierzchni liści rośliny") +
   theme_minimal() +
   scale_x_discrete(labels = mapowanie_dni)+
-  scale_y_continuous(limits = c(0, 100))# Ustawienie etykiet na osi X jako daty
+  scale_y_continuous(limits = c(0, 100))
 
 uprawy1
 
